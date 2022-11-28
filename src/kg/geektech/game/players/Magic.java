9e020e6a8172f8595073af1a04cff9c52410010a
AidaDurusbekova <aidadurusbekyzy@icloud.com>
2022-11-28 21:32:12 +0600

    hw

---
+++ src/kg/geektech/game/players/Magic.java
@@ -0,0 +1,19 @@
+package kg.geektech.game.players;
+
+public class Magic extends Hero {
+    public Magic(int health, int damage, String name) {
+        super(health, damage, SuperAbility.BOOST, name);
+    }
+
+    @Override
+    public void applySuperPower(Boss boss, Hero[] heroes) {
+        int boost = 10;
+        for (int i = 0; i < heroes.length; i++) {
+            if (heroes[i].getHealth() > 0 && this.getHealth() > 0 ){
+                heroes[i].setDamage(heroes[i].getDamage() + boost);
+            }
+
+        }
+
+    }
+}
